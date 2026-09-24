ccb70e0d0ae0477bca57808fac6eb1117da8c33f
SujalSmaurya2090-sketch <sandeepmaurya2090@gmail.com>
2026-09-25 01:09:34 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -512,7 +512,7 @@ def scan_qr_attendance():
         if not teacher_info:
             return jsonify({'status': 'error', 'message': 'Aapki Email Teacher Database mein nahi mili!'}), 403
 
-        t_sheet = sheet.worksheet("Teacher Master")
+        t_sheet = sheet.worksheet("Teacher_Master")
         t_name = teacher_info.get('Full_Name')
 
         # Check Duplicate Entry for Today
